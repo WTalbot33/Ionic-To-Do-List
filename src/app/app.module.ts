84c6712f211dc6973358a269b5b7f68c19f546b8
Will Talbot <WillTalbot@Wills-MBP.frontierlocal.net>
2018-11-12 14:28:23 -0600

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AddItemPage } from '../pages/add-item/add-item';
+import { ItemDetailPage } from '../pages/item-details/item-details';
+import { Data } from '../providers/data/data';
+import { StatusBar } from '../../node_modules/@ionic-native/status-bar';
+import { SplashScreen } from '../../node_modules/@ionic-native/splash-screen';
+ 
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    AddItemPage,
+    ItemDetailPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AddItemPage,
+    ItemDetailPage
+  ],
+  providers: [Data, {provide: ErrorHandler, useClass: IonicErrorHandler}, StatusBar, SplashScreen]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { ModalController, NavController } from 'ionic-angular';
+import { AddItemPage } from '../add-item/add-item'
+import { ItemDetailPage } from '../item-details/item-details';
+import { Data } from '../../providers/data/data';
+ 
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ 
+  public items = [];
+ 
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public dataService: Data) {
+ 
+    this.dataService.getData().then((todos) => {
+ 
+      if(todos){
+        this.items = todos;
+      }
+ 
+    });
+ 
+  }
+ 
+  ionViewDidLoad(){
+ 
+  }
+ 
+  addItem(){
+ 
+    let addModal = this.modalCtrl.create(AddItemPage);
+ 
+    addModal.onDidDismiss((item) => {
+ 
+          if(item){
+            this.saveItem(item);
+          }
+ 
+    });
+ 
+    addModal.present();
+ 
+  }
+ 
+  saveItem(item){
+    this.items.push(item);
+    this.dataService.save(this.items);
+  }
+ 
+  viewItem(item){
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
+  }
+ 
+  removeItem(){
+    this.items.pop();
+  }
+
+  editItem(item){
+    this.navCtrl.push(ItemDetailPage, {
+      
+    })
+  }
+}
